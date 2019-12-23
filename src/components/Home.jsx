c77a51cbcb9f30ca1f57ba2af408f57144e7f73c
RevanthGovindan <revanthji14@gmail.com>
2019-12-23 22:37:52 +0530

    updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,14 +21,23 @@ function Home(props) {
                     <div style={{ textAlign: "justify" }}>
                         Hi, I'm full stack developer, I enjoy building applications from scratch,I love to
                         solve complex problems in <a href="https://www.hackerrank.com/Revanth_G" target="_blank"
-                        rel="noopener noreferrer">
+                            rel="noopener noreferrer">
                             Hackerrank</a> and <a href="http://app.e-box.co.in" target="_blank"
-                            rel="noopener noreferrer">Ebox</a> platforms.
-                        I can proudly say that leadership is my best quality.
-                        My hobbies are listening to musics, watching movies, riding and travelling.
+                                rel="noopener noreferrer">Ebox</a> platforms.
+                I use primarily use Javascript(with frameworks) and Java,but I'm
+                ready to work on any language, I write reusable codes.
+I can proudly say that leadership is my best quality.
+My hobbies are listening to musics, watching movies and riding.
                     </div><br />
                     <span className="flex contact-link" onClick={() => props.changeTab()}>
                         Let's discuss
+                    </span>
+                    <br />
+                    <span className="flex contact-link">
+                        <a href="https://docs.google.com/document/u/0/d/1FtLL3BobScU0N0h4uAwfyJ4I59FXX_TrUfImcu0zOFc/export?format=pdf"
+                            target="_blank">
+                            Download me
+                        </a>
                     </span>
                 </div>
             </div>
--- src/constants.js
+++ src/constants.js
@@ -8,20 +8,17 @@ export const navBar = {
 };
 
 export const skills = [
+    { name: "React Js", mark: "90%" },
+    { name: "Node Js", mark: "85%" },
+    { name: "HTML 5", mark: "85%" },
     { name: "C", mark: "80%" },
+    { name: "CSS 3", mark: "80%" },
+    { name: "MongoDB", mark: "80%" },
     { name: "JAVA", mark: "70%" },
     { name: "Spring Boot", mark: "70%" },
-    { name: "Python", mark: "55%" },
-    { name: "Node Js", mark: "75%" },
-    { name: "Express Js", mark: "75%" },
-    { name: "Hapi Js", mark: "75%" },
-    { name: "HTML 5", mark: "85%" },
-    { name: "CSS 3", mark: "80%" },
-    { name: "React Js", mark: "80%" },
-    { name: "Redux Js", mark: "80%" },
-    { name: "Angular >4", mark: "50%" },
+    { name: "Angular >4", mark: "70%" },
     { name: "MySQL", mark: "70%" },
-    { name: "MongoDB", mark: "70%" }
+    { name: "Python", mark: "55%" }
 ];
 
 export const myWork = [
@@ -36,6 +33,10 @@ export const myWork = [
     {
         productName: "MSF Exams", technologies: "Spring Boot, React Js, MongoDB",
         description: "An web based application to make interview process much simpler."
+    },
+    {
+        productName: "SHIELD", technologies: "Java, React Js(Hooks), MySQL, RabbitMQ",
+        description: "Push notification console for admins, who can easily send push notifications for all, selected or based on user subscription. Best feature of this application is admin can selected based on platform like Android,IOS or Web. I have done integration with firebase, report generation for delivery, click and goal conversions, developed console UI for admins, also SDK to received push notification using serviceworkers."
     }
 ];
 
@@ -43,11 +44,11 @@ export const feedback = [
     {
         feedback: "Revanth is an excellent contributor to the project and hence the company. He takes interests to take up any responsibility and goes to any extent in fulfilling the task requirements. And Shows excellent capability and interest to learn and adapt to latest technologies and libraries. He is a technology enthusiast and often comes up with ideas and concepts of latest frameworks and technologies. He from this few months of experience has grabbed a handful of skills to both work in front-end and middleware technologies.  He is an effective communicator and possess good team working skills.",
         by: "Vijayakanth P", position: "Team Lead(Market Simplified)",
-        ratings:5
+        ratings: 5
     },
     {
         feedback: "Revanth has shown the highest levels of commitments  and ownership in taking up initiatives to complete tasks on time. He has been proactive on this especially when doing impact analysis on specific changes. That has helped in yielding better quality releases. He is also well aware of his boundaries and knows when to seek help. He is adaptable and has the willingness to learn. Never misses an opportunity to learn and contribute. Has been able to consistently perform under pressure. Has been a single man army for Frontend in a complex project like Indiabulls.",
         by: "Hiran Ramankutty", position: "Project Lead(Market Simplified)",
-        ratings:4
+        ratings: 4
     }
 ];
