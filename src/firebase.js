2f8ce6f5b248b351e8dd56eadc414565efd3a52a
Revanth G <revanthg@Revanths-MacBook-Pro.local>
2025-03-16 10:29:39 +0530

    version fixes

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,18 @@
-import firebase from "firebase";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getPerformance } from "firebase/performance";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAJ646BNkDzlIwTXsEDG80XpLgw8mqMh3Q",
-    authDomain: "portfolio-revanth.firebaseapp.com",
-    databaseURL: "https://portfolio-revanth.firebaseio.com",
-    projectId: "portfolio-revanth",
-    storageBucket: "portfolio-revanth.appspot.com",
-    messagingSenderId: "763993411291",
-    appId: "1:763993411291:web:c3155206adaa46bc"
+  apiKey: "AIzaSyAJ646BNkDzlIwTXsEDG80XpLgw8mqMh3Q",
+  authDomain: "portfolio-revanth.firebaseapp.com",
+  databaseURL: "https://portfolio-revanth.firebaseio.com",
+  projectId: "portfolio-revanth",
+  storageBucket: "portfolio-revanth.appspot.com",
+  messagingSenderId: "763993411291",
+  appId: "1:763993411291:web:c3155206adaa46bc",
 };
-firebase.initializeApp(firebaseConfig);
 
-export const db = firebase.firestore();
-export const fbPerformance = firebase.performance();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const fbPerformance = getPerformance(app);
